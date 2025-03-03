43edc8a58c01569f28925ab05219ebb4a614fd6a
mdekerga <mathieu.dekergar@gmail.com>
2025-03-03 10:34:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const apiKey = '';
+
+function getRandomYoutubeVideo(){
+
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=random&type=video&key=${apiKey}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const randomIndex = Math.floor(Math.random() * data.items.length);
+        const videoID = data.items[randomIndex].id.videoId;
+
+        const iframe = document.querySelector('#video-frame');
+        iframe.src = `https://www.youtube.com/embed/${videoID}`;
+        console.log(videoID);
+        console.log(data);
+    })
+    .catch(error => {
+        console.error("Erreur lors de la récupérations des vidéos Youtube",error)
+    });
+
+
+}
+
+document.querySelector('.button').addEventListener('click',getRandomYoutubeVideo);
